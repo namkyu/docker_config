35e1a2034f2843be5bb5b0fee6563636b443a6e3
namkyu <nklee@ncsoft.com>
2020-02-26 10:42:01 +0900

    superset

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -1,1 +1,11 @@
-SQLALCHEMY_DATABASE_URI = 'mysql://nklee:abcde1234@127.0.0.1:3306/supersetDB'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:aaaa1111#@localhost:3306/SupersetDb'
+
+CACHE_CONFIG = {
+    'CACHE_TYPE': 'redis',
+    'CACHE_DEFAULT_TIMEOUT': 300,
+    'CACHE_KEY_PREFIX': 'superset_',
+    'CACHE_REDIS_HOST': 'localhost',
+    'CACHE_REDIS_PORT': 6379,
+    'CACHE_REDIS_DB': 1,
+    'CACHE_REDIS_URL': 'redis://localhost:6379/1'
+}
